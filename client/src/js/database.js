8bfd01f33990d0326f09eae82552d4076fbcfa56
Traspo <nyaspo8@gmail.com>
2024-02-20 15:14:06 -0500

    all seems to be working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,14 +14,18 @@ const initdb = async () =>
   });
 
 // Method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const postDb = async (content) => {
   console.log('Post to the database');
 
+  if (!content) {
+    await content
+  }
+
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
@@ -34,7 +38,7 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 
   if (!result) {
-    console.error('putDb not implemented');
+    console.error('postDb not implemented');
   }
 };
 
@@ -42,26 +46,22 @@ export const putDb = async (content) => {
 export const getDb = async () => {
 
   // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('content', 1);
+  const jateDb = await openDB('jate', 1);
+  // console.log(jateDb);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('content', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
-
-  // if (!result) {
-  //   console.error('putDb not implemented');
-  // }
-  // console.error('getDb not implemented');
+  // console.log('result.value', result[0].content);
+  return result[0];
 };
 
 
